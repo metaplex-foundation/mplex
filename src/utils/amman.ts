79121d64dca6dcc43fc1bbc5ff5fa8d5e76dc42b
Thorsten Lorenz <thlorenz@gmx.de>
2022-10-04 14:02:13 -0600

    util: setup amman wrapper that is only sometimes enabled

---
+++ src/utils/amman.ts
@@ -0,0 +1,40 @@
+import { Amman } from '@metaplex-foundation/amman-client'
+import { ClusterWithLocal } from '../types'
+
+const programIds = {
+  hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk: 'auctionHouse',
+  CnDYGRdU51FsSyLnVgSd19MCFxA4YHT5h3nacvCKMPUJ: 'candyGuard',
+  CndyV3LdqHUfDLmE5naZjVN8rBZz4tqhdefbAnjHG3JR: 'candyMachineCore',
+  metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s: 'tokenMetadata',
+  gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs: 'solanaGateway',
+}
+
+export class MplexAmman {
+  private readonly _amman: Amman | undefined
+
+  private constructor(cluster: ClusterWithLocal) {
+    // At this point we require the MPLEX_AMMAN env var to be set, but we could
+    // also try to connect to amman relay and enable it magically.
+    // Alternatively we could provide a wrapper binary which calls `mplex` with
+    // that env var set.
+    if (cluster === 'local' && process.env.MPLEX_AMMAN != null) {
+      this._amman = Amman.instance({
+        knownLabels: programIds,
+      })
+    }
+  }
+
+  get amman(): Amman | undefined {
+    return this._amman
+  }
+
+  private static _instance: MplexAmman | undefined
+  static init(cluster: ClusterWithLocal) {
+    MplexAmman._instance = new MplexAmman(cluster)
+  }
+  static get amman(): Amman | undefined {
+    return MplexAmman._instance?.amman
+  }
+}
+
+export const amman = () => MplexAmman.amman
